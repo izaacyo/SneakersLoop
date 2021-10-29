146967275b45d94a224b232166fbc5dc53e6d498
izaacyo <mariusppetrut@gmail.com>
2021-10-29 15:35:07 +0300

    created activation route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const userCtrl = require("../controllers/userCtrl")
 
+
 router.post('/register', userCtrl.register)
+
+router.post('/activation', userCtrl.activateEmail)
 
 
 module.exports = router
